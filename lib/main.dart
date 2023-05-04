b440a7fdf9c7b1a1892d4010de34454112b7a641
farukmrt <farukmrt1@gmail.com>
2023-05-04 03:05:52 +0300

    gun_sonu

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
-
-import 'dart:convert';
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-void void main() {
-  runApp();
-  
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 251, 255, 0),
+                Colors.black,
+                Color.fromARGB(255, 255, 25, 21)
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text('sonunda_yazdiik'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
